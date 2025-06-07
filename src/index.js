4748dde417680aeab1c570d1804b0131b383b322
Jain Rishit Sanjay <73696968+Rachet2006@users.noreply.github.com>
2025-06-07 17:16:23 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import NewsletterLandingPage from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <NewsletterLandingPage />
+  </React.StrictMode>
+);
